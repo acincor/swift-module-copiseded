e58733b3abee29fec27b024d1246201b8f5fbe58
monkeyhammercopiseded <103583956+monkeyhammercopiseded@users.noreply.github.com>
2022-05-29 12:54:15 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 import setuptools
 
-with open("README.rst", "r", encoding="utf-8") as fh:
+with open("README.rst", "r",encoding = "UTF-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="swift-module-copiseded",
-    version="0.5.2",
+    version="0.6.0",
     author="Monkey Hammer Copiseded",
     author_email="Wf6350177@163.com",
-    description="you can use \"python3 -m pip swift-module-copiseded --upgrade\" to update!!!,and I write some new some new project(squareRoot,squareOfANumber",
+    description="this is swift-module-copiseded 0.6.0，kill some bug",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://pypi.org/project/swift-module-copiseded/",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/monkeyhammercopiseded/swift-module-copiseded-0.6.0/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
